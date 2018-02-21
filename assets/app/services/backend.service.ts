0b401e909dff7003ea1e79f98406754eb28f148c
adzina <adzina3@vp.pl>
2018-02-21 16:25:13 +0100

    docker's localhost address added

--- assets/app/services/backend.service.ts
+++ assets/app/services/backend.service.ts
@@ -18,7 +18,8 @@ import * as async from "async";
 
 export class BackendService{
 
-  g_url='http://localhost:1337/';
+  g_url='http://192.168.99.100:1337/';
+
 
   constructor(private http:AuthHttp,
               private _http:Http,
