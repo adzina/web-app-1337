a5b17f3127329200063584a0cfb0abead7d857d6
adzina <adzina3@vp.pl>
2018-05-01 08:10:48 +0200

    pagination added to AdminGroup

--- assets/app/components/AdminGroupComponent/adminGroupComponent.component.ts
+++ assets/app/components/AdminGroupComponent/adminGroupComponent.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { BackendService } from '../../services/backend.service';
 import { LoginService } from '../../services/login.service';
+import { PagerService } from '../../services/pager.service';
 import { Router } from '@angular/router';
 import { Group } from '../../models/group';
 
@@ -15,13 +16,17 @@ export class AdminGroupComponent{
   chosenGroup: Group;
   groups: Group[];
   dropdownText:string;
+  pager: any = {};
+  pagedGroups: Group[];
   constructor(private _backendService: BackendService,
               private _loginService: LoginService,
+              private _pagerService: PagerService,
               private _router:Router) {
             this.groups = null;
             _backendService.getAllGroups().
               subscribe(response=>{
                 this.groups=response;
+                this.setPage(1);
                   }
                 );
       }
@@ -29,13 +34,22 @@ export class AdminGroupComponent{
       this._router.navigate(["./admin-create-group"]);
     }
     addRemove(nr:string) {
-      var group=this.groups[nr];
-      console.log(group)
+      var group=this.pagedGroups[nr];
       this._loginService.setChosenGroup(group);
       this._router.navigate(['./admin-add-users']);
     }
+    setPage(page: number) {
+     if (page < 1) {
+       return;
+     }
+     // get pager object from service
+     this.pager = this._pagerService.getPager(this.groups.length, page);
+     // get current page of items
+     this.pagedGroups = this.groups.slice(this.pager.startIndex,
+       this.pager.endIndex + 1);
+   }
     delete(i:number){
-      let group = this.groups[i]
+      let group = this.pagedGroups[i]
       this._backendService.deleteGroup(group.id).subscribe(data=>{
         this._router.navigate(['./admin-group'])
       })
