8349848c63723f4b3527841e38c80f9518d991d5
adzina <adzina3@vp.pl>
2018-05-11 13:04:38 +0200

    progress added to the group view

--- assets/app/components/TeacherSeeGroupsLessonsComponent/teacherSeeGroupsLessons.component.ts
+++ assets/app/components/TeacherSeeGroupsLessonsComponent/teacherSeeGroupsLessons.component.ts
@@ -3,6 +3,7 @@ import { BackendService } from '../../services/backend.service';
 import { LoginService } from '../../services/login.service';
 import {Lesson} from '../../models/lesson';
 import { Group } from '../../models/group';
+import { User } from '../../models/user';
 import { Router } from '@angular/router';
 
 @Component({
@@ -17,13 +18,22 @@ export class TeacherSeeGroupsLessonsComponent {
   className:string;
   backendError:string;
   group:Group;
-
+  students:User[];
+  sortedStudents:User[]
+  groupChosen=false;
+  progress:number[];
+  progress2:number[];
   constructor(private _backendService:BackendService,
               private _loginService:LoginService,
               private _router:Router) {
     this.group = this._loginService.getChosenGroup();
     this.lessons=[];
     this.backendError=null;
+    this.students=[];
+    this.progress=[];
+    this.progress2=[];
+    this.group = _loginService.getChosenGroup();
+    this.getData();
 
     _backendService.getGroupsLessons(this.group.id).
         subscribe(response=>{
@@ -54,5 +64,66 @@ export class TeacherSeeGroupsLessonsComponent {
   seeGroup(){
     this._router.navigate(['./see-groups']);
 
+  }
+  isStudent(user){
+    for(let role of user.role){
+      if(role=="admin")
+        return false;
+    }
+    for(let role of user.role){
+      if(role=="student")
+        return true;
+    }
+    return false;
+  }
+  getData(){
+    this._backendService.getActiveUsers(this.group.id).subscribe(
+      data=>{
+           for(let user of data){
+             let ids=[]
+             if(this.isStudent(user)){
+                this.students.push(user);
+                ids.push(user.id);
+             }
+             this.sortedStudents = this.students.sort((obj1, obj2) => {
+               if (obj1.id > obj2.id) return 1
+               if (obj1.id < obj2.id) return -1
+               return 0;
+             });
+             this._backendService.countWordsForManyStudents(ids, this.group.id,1)
+             .subscribe(data=>{
+               var sortedData = data.sort((obj1, obj2) => {
+                 if (obj1.studentID > obj2.studentID) return 1
+                 if (obj1.studentID < obj2.studentID) return -1
+                 return 0;
+               });
+               for (let record of sortedData){
+                 if(record.all!=0)
+                      this.progress.push(record.guessed/record.all)
+                  else
+                      this.progress.push(0)
+               }
+               this._backendService.countWordsForManyStudents(ids, this.group.id,4)
+               .subscribe(data1=>{
+                 var sortedData1 = data1.sort((obj1, obj2) => {
+                   if (obj1.studentID > obj2.studentID) return 1
+                   if (obj1.studentID < obj2.studentID) return -1
+                   return 0;
+                 });
+                 for (let record of sortedData1){
+                   if(record.all!=0)
+                        this.progress2.push(record.guessed/record.all)
+                    else
+                        this.progress2.push(0)
+                 }
+               })
+
+
+             }
+             )
+
+           }
+      }
+    )
   }
 }
